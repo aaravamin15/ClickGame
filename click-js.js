c47b74e8f1a5f20145df6d856330e37a0d74c9b6
Alisa1098 <118067084+Alisa1098@users.noreply.github.com>
2023-11-01 13:02:38 -0400

    comments

---
+++ click-js.js
@@ -0,0 +1,18 @@
+document.getElementById("target").style.display = 
+document.getElementById("start").addEventListener("click", start);
+
+//starts when start button is clicked
+function start(){
+        var e = document.getElementById("target");
+        if(e.style.display == 'block')
+           e.style.display = 'none';
+        else
+           e.style.display = 'block';
+         //set a timer variable 
+}
+
+// function to make the dot appear/disapear every 5 secs
+// set amount of time to grow larger when they appear & time to shrink until they disappear
+// function that ads to a counter everytime the dot is clicked
+// end screen- called whent eh timer runs out (printing counter, play again button, etc)
+
